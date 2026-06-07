438adcf66b4653e4652fa4d1b3646088a43e7905
shiro8613 <psp1070@gmail.com>
2026-06-07 22:16:07 +0900

    実装迷走により処理をリセット

--- main.c
+++ main.c
@@ -15,8 +15,6 @@
 #define UNKNOWN 0xFF
 #define OPCODES_LEN 16
 #define OPCODES_TYPE_LEN 2
-#define OPCODES_REG 0
-#define OPCODES_LIT 1
 
 typedef struct opcode_t {
   const char *name;
@@ -91,13 +89,13 @@ void to_lower(char *buf) {
     return;
   }
 
-  for (int i = 0; buf[i] != '\0'; i++) {
+  for (size_t i = 0; buf[i] != '\0'; i++) {
     buf[i] = tolower(buf[i]);
   }
 }
 
 typedef struct var_buffer {
-  long *ptr;
+  uint8_t *ptr;
   size_t capa;
   size_t len;
 } var_buffer;
@@ -107,7 +105,7 @@ var_buffer *init_var_buffer() {
   if (instance) {
     instance->len = 0;
     instance->capa = 2;
-    instance->ptr = (long *)malloc(sizeof(long) * instance->capa);
+    instance->ptr = (uint8_t *)malloc(sizeof(uint8_t) * instance->capa);
   }
 
   return instance;
@@ -118,112 +116,23 @@ void free_var_buffer(var_buffer *instance) {
   free(instance);
 }
 
-void append_var_buffer(var_buffer *instance, long value) {
+void append_var_buffer(var_buffer *instance, uint8_t value) {
   if (instance->len >= instance->capa) {
     instance->capa *= 2;
     instance->ptr =
-        (long *)realloc(instance->ptr, sizeof(long) * instance->capa);
+        (uint8_t *)realloc(instance->ptr, sizeof(uint8_t) * instance->capa);
   }
 
   instance->ptr[instance->len++] = value;
 }
 
-long get_var_buffer(var_buffer *instance, size_t idx) {
+uint8_t get_var_buffer(var_buffer *instance, size_t idx) {
   if (instance->len < idx) {
     return -1;
   }
 
   return instance->ptr[idx];
 }
-
-typedef enum ParseState {
-  WAIT,
-  LITERAL,
-  OK,
-} ParseState;
-
-typedef enum ParseResult {
-  CONTINUE,
-  SUCCESS,
-  FAIL,
-} ParseResult;
-
-struct op_parser {
-  ParseState state;
-  struct opcode_i opcode;
-  uint8_t opcode_len;
-  uint8_t reg;
-  long from;
-};
-
-void parser_clear(struct op_parser *p) {
-  opcode_clear(&p->opcode);
-  p->state = WAIT;
-  p->opcode_len = 0;
-  p->reg = 0x00;
-  p->from = 0x00;
-}
-
-// todo: opcodes毎に変数長が違う問題を何とかする
-// 現在は初手レジスタ固定だが、その限りでは無いものの対応をする
-
-ParseResult parser_parse(struct op_parser *p, char *buf) {
-  if (p->reg == 0x00) {
-    if (p->state == WAIT) {
-      if (opcode_find(&p->opcode, buf)) {
-        return FAIL;
-      }
-      p->state = LITERAL;
-    } else if (p->state == LITERAL) {
-      uint8_t r = to_register(buf);
-      if (r == UNKNOWN) {
-        char *endptr;
-        long l = strtol(buf, &endptr, 0);
-        if (buf != endptr) {
-          p->from = l;
-          p->opcode_len++;
-          return CONTINUE;
-        }
-
-        return FAIL;
-      }
-
-      p->reg = r;
-      p->opcode_len++;
-    }
-
-    return CONTINUE;
-  } else if (p->from == 0x00 && p->opcode_len <= p->opcode.len) {
-    long l;
-    char *endptr;
-    l = strtol(buf, &endptr, 0);
-    if (buf != endptr) {
-      p->from = l;
-    } else {
-      uint8_t r = to_register(buf);
-      if (r == UNKNOWN) {
-        return FAIL;
-      }
-      p->from = r;
-    }
-
-    return SUCCESS;
-  } else if (p->opcode_len >= p->opcode.len) {
-    return SUCCESS;
-  }
-
-  return CONTINUE;
-}
-
-void parser_print(struct op_parser *p) {
-  printf("%ld %d %ld\n", p->opcode.idx, p->reg, p->from);
-}
-
-typedef enum Mode {
-  SKIP,
-  LIT,
-  COMMENT,
-} Mode;
 
 bool is_valid_char(char c) {
   return isprint((unsigned char)c) && !isspace((unsigned char)c);
@@ -233,56 +142,7 @@ bool is_comment(char *buf, size_t len, size_t pos) {
   return buf[pos] == '/' && pos < len && buf[pos + 1] == '/';
 }
 
-int parse_text(char *buf, ssize_t len) {
-
-  Mode mode = SKIP;
-  size_t lit_len = 0;
-  char lit_buf[LIT_BUFF_SIZE];
-  struct op_parser parser = {0};
-  parser_clear(&parser);
-
-  for (ssize_t i = 0; i < len; i++) {
-    char c = buf[i];
-
-    switch (mode) {
-    case SKIP:
-      if (is_comment(buf, len, i)) {
-        mode = COMMENT;
-        continue;
-      }
-      if (is_valid_char(c)) {
-        mode = LIT;
-        lit_buf[lit_len++] = c;
-      }
-      break;
-    case LIT:
-      if (is_valid_char(c)) {
-        lit_buf[lit_len++] = c;
-      } else {
-        lit_buf[lit_len++] = '\0';
-        to_lower(lit_buf);
-        ParseResult res = parser_parse(&parser, lit_buf);
-        if (res == FAIL) {
-          return -1;
-        } else if (res == SUCCESS) {
-          parser_print(&parser);
-          parser_clear(&parser);
-        }
-
-        mode = SKIP;
-        lit_len = 0;
-      }
-      break;
-    case COMMENT:
-      if (c == '\n') {
-        mode = SKIP;
-      }
-      break;
-    }
-  }
-
-  return 0;
-}
+int parse_text(char *buf, ssize_t len) { return 0; }
 
 int main(void) {
   FILE *fp = fopen(FILE_NAME, "r");
